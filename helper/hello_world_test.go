fe69fce4232b193aa356f5b7a3f3c4167dc47ec0
badrules <ahmad.badrul@vcgamers.com>
2023-01-07 14:21:12 +0700

    testing dengan flesksibel dengan hanya menambahkan data

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -58,3 +58,44 @@ func TestSubTest(t *testing.T) {
 		require.Equal(t, "Hellosss Khannedy", result, "Result must be 'Hello Khannedy'")
 	})
 }
+
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Eko",
+			request:  "Eko",
+			expected: "Hello Eko",
+		},
+		{
+			name:     "Kurniawan",
+			request:  "Kurniawan",
+			expected: "Hello Kurniawan",
+		},
+		{
+			name:     "Khannedy",
+			request:  "Khannedy",
+			expected: "Hello Khannedy",
+		},
+		{
+			name:     "Budi",
+			request:  "Budi",
+			expected: "Hello Budi",
+		},
+		{
+			name:     "Joko",
+			request:  "Joko",
+			expected: "Hello Joko",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
