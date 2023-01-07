b6142593757d9d42aaff06ec5f00df01edcbe1b2
badrules <ahmad.badrul@vcgamers.com>
2023-01-07 13:43:14 +0700

    assert

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,11 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Bad")
@@ -14,4 +19,10 @@ func TestBad(t *testing.T) {
 	if result != "Hello Bad" {
 		t.Errorf("HelloWorld() = %s; want Hello Bad", result)
 	}
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Assert Done")
 }
